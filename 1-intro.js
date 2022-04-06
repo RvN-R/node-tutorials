305971d90eab5938e18eeb6e40358d3929ea0b2d
Richard Van Nairn <rvannairn@googlemail.com>
2022-04-06 15:00:48 +0100

    1-intro.js completed

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 12
+
+if(amount < 10){
+    console.log("Small Number")
+}else{
+    console.log("Large Number")
+}
+
+console.log("Hey Ites My First Node App!!")
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # node-tutorials
+
+Working through a Node.js and Express.js Tutorial on freecodeCamp to increase my understanding and tech stack!
