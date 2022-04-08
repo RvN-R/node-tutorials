f196bd5140e3ad101550bc7b440b3b1f82ef69cf
Richard Van Nairn <rvannairn@googlemail.com>
2022-04-08 11:09:05 +0100

    Http module completed

---
+++ app.js
@@ -0,0 +1,9 @@
+// npm - global command, comes with node
+// npm --version
+
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo install -g <packageName> (mac) 
