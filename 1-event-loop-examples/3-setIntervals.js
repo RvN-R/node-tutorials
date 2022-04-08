d9d1d58512b69861ecead600372a418fec790786
Richard Van Nairn <rvannairn@googlemail.com>
2022-04-08 12:36:05 +0100

    Completed Event Loop Examples

---
+++ 1-event-loop-examples/3-setIntervals.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    console.log('hello world')
+},2000)
+console.log('I will run first')
+//process stat alive unless
+//Kill process CONTROL + C
+// unexpected error
--- app.js
+++ app.js
@@ -6,4 +6,18 @@
 
 // global dependency - use it in any project
 // npm install -g <packageName>
-// sudo install -g <packageName> (mac) 
+// sudo npm install -g <packageName> (mac) 
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+
+// const _ = require('lodash');
+
+// const items = [1,[2,[3,[4]]]]
+// const newItems = _.flattenDeep(items);
+// console.log(newItems)
+// console.log('hello people')
+
